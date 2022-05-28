b5573f78018637e25da8a762085c3823eb232e6e
Łukasz Wira <lukasz.wira@gmail.com>
2022-05-28 21:49:27 +0200

    add placeholder

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     movies = []
+    for movie in range(1,9):
+        movies.append([])
     return render_template("homepage.html", movies=movies)
