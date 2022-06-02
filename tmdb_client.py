4939992bf2de1d76f32f8d93bdaf20d1a0a6d735
Łukasz Wira <lukasz.wira@gmail.com>
2022-06-02 21:27:54 +0200

    movie detail page

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,13 +1,18 @@
 import requests
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGU4ODRmNzM3MWMzNDIwM2Y3MzZmZWNjODIzNmE1ZCIsInN1YiI6IjYyOTA4YzY1N2Q1ZGI1MTBhNTFlNzFlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bCSFcue4zW-DLWIZMa5HULuJV-b5BwW2C5t2jAFrvOg"
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGU4ODRmNzM3MWMzNDIwM2Y3MzZmZWNjODIzNmE1ZCIsInN1YiI6IjYyOTA4YzY1N2Q1ZGI1MTBhNTFlNzFlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bCSFcue4zW-DLWIZMa5HULuJV-b5BwW2C5t2jAFrvOg"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
+    
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
+    return data["results"][:how_many]
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
@@ -17,4 +22,27 @@ def get_movie_info(original_title):
     title = "original_title"
     return f"{title}/{original_title}"
 
+def get_single_movie(movie_id):
+    endpoint = "https://api.themoviedb.org/3/movie/" + movie_id
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
 
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
